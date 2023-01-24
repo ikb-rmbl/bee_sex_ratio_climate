3051111f7cf0d7b271ce6ddd0ebfb31c590808d0
Ian Breckheimer <ikb@rmbl.org>
2023-01-24 09:56:59 -0700

    Added plots and updated snow onset column.

--- code/extract_bee_site_climate_ts.R
+++ code/extract_bee_site_climate_ts.R
@@ -24,13 +24,13 @@ sdp_cat <- sdp_get_catalog(domains="UG",
                            deprecated=FALSE,
                            return_stac=FALSE)
 
-##Gets the appropriate Catalog ID for each product.
+## Gets the appropriate Catalog ID for each product.
 snow_onset_catID <- sdp_cat$CatalogID[sdp_cat$Product=="Snow Onset Day of Year Yearly Timeseries"]
 snow_persist_catID <- sdp_cat$CatalogID[sdp_cat$Product=="Snow Persistence Day of Year Yearly Timeseries"]
 tmax_catID <- sdp_cat$CatalogID[sdp_cat$Product=="Maximum 2m Air Temperature Daily Timeseries"]
 tmin_catID <- sdp_cat$CatalogID[sdp_cat$Product=="Minimum 2m Air Temperature Daily Timeseries"]
 
-##Gets just a few layers of each dataset to test.
+## Grabs snow products for the appropriate years.
 bee_years <- 2006:2022
 snow_onset_rast <- sdp_get_raster(catalog_id = snow_onset_catID,
                                   years=bee_years)
@@ -63,6 +63,12 @@ sites_snow_onset_long <- pivot_longer(sites_snow_onset,
 sites_snow_long <- rbind(sites_snow_persist_long,sites_snow_onset_long)
 sites_snow_wide <- pivot_wider(sites_snow_long,names_from="Dataset",values_from="value")
 sites_snow_wide$SnowLengthDays <-  sites_snow_wide$SnowPersistDOY - sites_snow_wide$SnowOnsetDOY
+
+##Rescales snow onset to calendar year and DOY.
+SnowOnsetDOY_df <- data.frame(Site=sites_snow_wide$Site,
+                              SnowOnsetDOY_calendar=sites_snow_wide$SnowOnsetDOY + 365,
+                              Year=as.character(as.numeric(sites_snow_wide$Year) - 1))
+sites_snow_wide <- left_join(sites_snow_wide,SnowOnsetDOY_df,by=c("Site","Year"))
 
 ## Writes snow data to disk.
 write.csv(sites_snow_wide,"./output/bee_sites_snow_2006_2022.csv")
@@ -121,7 +127,8 @@ for (i in 1:length(unique_years)){
 extracted_temp_df <- bind_rows(extracted_temps)
 write.csv(extracted_temp_df,file="./output/bee_sites_temp_2006_2022.csv")
 
-##Plots temp data for sanity check.
+
+##Computes weekly quantiles.
 extracted_temp_df$Date <- as.Date(extracted_temp_df$Date)
 extracted_temp_df$DOY <- as.numeric(format(extracted_temp_df$Date,format="%j"))
 extracted_temp_df$Year <- format(extracted_temp_df$Date,format="%Y")
@@ -133,5 +140,63 @@ ggplot(extracted_temp_df)+
   #scale_x_date(limits=as.Date(c("2007-01-01","2007-12-31")))+
   facet_wrap(facets=~Site)+
   theme_bw()
+
+## Computes weekly quantiles.
+extracted_temp_df$Week <- format(extracted_temp_df$Date,format="%W")
+
+weekly_sum <- extracted_temp_df %>% group_by(Site,Week) %>% 
+  summarise(Tmin_quant_min=min(Tmin),
+            Tmin_quant_025=quantile(Tmin,probs=c(0.025)),
+            Tmin_quant_10=quantile(Tmin,probs=c(0.1)),
+            Tmin_quant_25=quantile(Tmin,probs=c(0.25)),
+            Tmin_quant_50=quantile(Tmin,probs=c(0.5)),
+            Tmin_quant_75=quantile(Tmin,probs=c(0.75)),
+            Tmin_quant_90=quantile(Tmin,probs=c(0.9)),
+            Tmin_quant_975=quantile(Tmin,probs=c(0.975)),
+            Tmin_quant_max=max(Tmin),
+            Tmax_quant_min=min(Tmax),
+            Tmax_quant_025=quantile(Tmax,probs=c(0.025)),
+            Tmax_quant_10=quantile(Tmax,probs=c(0.1)),
+            Tmax_quant_25=quantile(Tmax,probs=c(0.25)),
+            Tmax_quant_50=quantile(Tmax,probs=c(0.5)),
+            Tmax_quant_75=quantile(Tmax,probs=c(0.75)),
+            Tmax_quant_90=quantile(Tmax,probs=c(0.9)),
+            Tmax_quant_975=quantile(Tmax,probs=c(0.975)),
+            Tmax_quant_max=max(Tmax))
+
+extracted_temp_df <- left_join(extracted_temp_df,weekly_sum,by=c("Site","Week"))
+
+##Joins temp and snow data.
+extracted_temp_df <- left_join(extracted_temp_df,sites_snow_wide,by=c("Site","Year"))
+
+##Creates plots for every year.
+for(i in 1:length(unique_years)){
+  
+  print(paste("Plotting year", unique_years[i]))
+  
+  p1 <- ggplot(filter(extracted_temp_df,Year == unique_years[i]))+
+    labs(title=paste("Air Temperature",unique_years[i]),y="Air Temp. (C)")+
+    geom_abline(aes(intercept=0,slope=0),linetype="dotted")+
+    geom_ribbon(aes(x=DOY,ymin=Tmax_quant_min,ymax=Tmax_quant_max),fill="darkred",alpha=0.1)+
+    geom_ribbon(aes(x=DOY,ymin=Tmax_quant_10,ymax=Tmax_quant_90),fill="darkred",alpha=0.25)+
+    #geom_ribbon(aes(x=DOY,ymin=Tmax_quant_25,ymax=Tmax_quant_75),fill="darkred",alpha=0.5)+
+    #geom_line(aes(x=DOY,y=Tmax_quant_50),color="darkred",alpha=0.25)+
+    geom_ribbon(aes(x=DOY,ymin=Tmin_quant_min,ymax=Tmin_quant_max),fill="darkblue",alpha=0.1)+
+    geom_ribbon(aes(x=DOY,ymin=Tmin_quant_10,ymax=Tmin_quant_90),fill="darkblue",alpha=0.25)+
+    #geom_ribbon(aes(x=DOY,ymin=Tmin_quant_25,ymax=Tmin_quant_75),fill="darkblue",alpha=0.5)+
+    #geom_line(aes(x=DOY,y=Tmin_quant_50),color="darkblue",alpha=0.25)+
+    geom_line(aes(x=DOY,y=Tmax,linetype=Year),color="darkred")+
+    geom_line(aes(x=DOY,y=Tmin,linetype=Year),color="darkblue")+
+    facet_wrap(facets=~Site)+
+    theme_bw()+
+    theme(legend.position="none")
+  
+  plot_filename <- paste0("./figs/air_temp_daily_allsites_year_",unique_years[i],".pdf")
+  pdf(plot_filename,width=12,height=10)
+  print(p1)
+  dev.off()
+}
+
+  
   
 
